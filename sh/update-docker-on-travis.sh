345e53df14ddf7e172683db1abca48f0392a73eb
Ryan Van Oss <ryan.vanoss@gmail.com>
2017-11-11 20:23:29 -0800

    docs(sh): Add link to Travis docs for updating Docker.

--- sh/update-docker-on-travis.sh
+++ sh/update-docker-on-travis.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 
+# @see https://docs.travis-ci.com/user/docker/#Installing-a-newer-Docker-version
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
