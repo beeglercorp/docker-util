4fa32328609ce2a67949acb3245173b70a9a3b72
Ryan Van Oss <ryan.vanoss@gmail.com>
2017-11-25 23:02:11 -0800

    feat(sh): Reduce logging in shell scripts by removing set -ev across the board.
    Our scripts log verbosely. No need for the extra logging, as it just clutters the CI logs.

--- sh/compare-images.sh
+++ sh/compare-images.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 
-# recommended by travis-ci
-# @see https://docs.travis-ci.com/user/customizing-the-build/#Implementing-Complex-Build-Steps
-set -ev
-
-
 if [ $# -ne 2 ]; then
   echo "This script expects 2, and only 2, arguments, but received $#."
   exit 1
--- sh/install-docker-on-trusty.sh
+++ sh/install-docker-on-trusty.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 
-# recommended by travis-ci
-# @see https://docs.travis-ci.com/user/customizing-the-build/#Implementing-Complex-Build-Steps
-set -ev
-
 # @see https://docs.travis-ci.com/user/docker/#Installing-a-newer-Docker-version
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
